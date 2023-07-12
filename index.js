2083e67bd953be57f56836ea822cd0db00fcfbee
bosskaboss <dibyeshsahoo02@gmail.com>
2023-07-12 11:47:23 -0700

    tying to make a line

--- index.js
+++ index.js
@@ -1,33 +1,27 @@
-// Settings
-var particleCount = 40,
-flareCount = 10,
-motion = 0.05,
-tilt = 0.05,
-color = '#FFEED4',
-particleSizeBase = 1,
-particleSizeMultiplier = 0.5,
-flareSizeBase = 100,
-flareSizeMultiplier = 100,
-lineWidth = 1,
-linkChance = 75, // chance per frame of link, higher = smaller chance
-linkLengthMin = 5, // min linked vertices
-linkLengthMax = 7, // max linked vertices
-linkOpacity = 0.25; // number between 0 & 1
-linkFade = 90, // link fade-out frames
-linkSpeed = 1, // distance a link travels in 1 frame
-glareAngle = -60,
-glareOpacityMultiplier = 0.05,
-renderParticles = true,
-renderParticleGlare = true,
-renderFlares = true,
-renderLinks = true,
-renderMesh = false,
-flicker = true,
-flickerSmoothing = 15, // higher = smoother flicker
-blurSize = 0,
-orbitTilt = true,
-randomMotion = true,
-noiseLength = 1000,
-noiseStrength = 1;
 
-var canvas = document.getElementById('stars')
+function drawLine(ctx, x1, y1, x2, y2, stroke = 'black', width = 3) {
+        let canvas = document.getElementById('canvas'),
+        ctx = canvas.getContext('2d');
+        // start a new path
+        ctx.beginPath();
+    
+        // place the cursor from the point the line should be started 
+        ctx.moveTo(x1, y1);
+    
+        // draw a line from current cursor position to the provided x,y coordinate
+        ctx.lineTo(x2, y2);
+    
+        // set strokecolor
+        ctx.strokeStyle = stroke;
+    
+        // set lineWidht 
+        ctx.lineWidth = width;
+    
+        // add stroke to the line 
+        ctx.stroke();
+    }
+function init() {
+    drawLine(ctx, 10, 10, 100, 100, 'red', 3);
+}
+
+
