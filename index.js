2c3033f23c83cedc095c56c1976dab9e1b1789b4
bosskaboss <dibyeshsahoo02@gmail.com>
2023-07-13 00:09:42 -0700

    i made a line

--- index.js
+++ index.js
@@ -1,27 +1,31 @@
-
-function drawLine(ctx, x1, y1, x2, y2, stroke = 'black', width = 3) {
-        let canvas = document.getElementById('canvas'),
-        ctx = canvas.getContext('2d');
-        // start a new path
-        ctx.beginPath();
-    
-        // place the cursor from the point the line should be started 
-        ctx.moveTo(x1, y1);
-    
-        // draw a line from current cursor position to the provided x,y coordinate
-        ctx.lineTo(x2, y2);
-    
-        // set strokecolor
-        ctx.strokeStyle = stroke;
-    
-        // set lineWidht 
-        ctx.lineWidth = width;
-    
-        // add stroke to the line 
-        ctx.stroke();
-    }
+var canvas = document.getElementById('myCanvas');
+var ctx = canvas.getContext('2d');
+var posY = 0;
+var posX = 0;
+var lineLength = 10;
+var speed = 2;
 function init() {
-    drawLine(ctx, 10, 10, 100, 100, 'red', 3);
+    ctx.clearRect(0,0,canvas.width, canvas.height);
+    drawLine(ctx, posX, posY, 100, 100, 'tomato', 3);
+}
+function drawLine(ctx, x1, y1, x2, y2, stroke = 'black', width = 3) {
+    // start a new path
+    ctx.beginPath();
+
+    // place the cursor from the point the line should be started 
+    ctx.moveTo(x1, y1);
+
+    // draw a line from current cursor position to the provided x,y coordinate
+    ctx.lineTo(x2, y2);
+
+    // set strokecolor
+    ctx.strokeStyle = stroke;
+
+    // set lineWidht 
+    ctx.lineWidth = width;
+
+    // add stroke to the line 
+    ctx.stroke();
 }
 
-
+if (canvas) init();
