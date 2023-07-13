0b81ef06a7686ed0ec7a4833c845a99510eadb89
bosskaboss <dibyeshsahoo02@gmail.com>
2023-07-13 11:21:34 -0700

    i got lines to move!!!!!

--- index.js
+++ index.js
@@ -1,12 +1,52 @@
 var canvas = document.getElementById('myCanvas');
 var ctx = canvas.getContext('2d');
-var posY = 0;
-var posX = 0;
-var lineLength = 10;
-var speed = 2;
+var iposY;
+var iposX;
+var lineLength = 200;
+var fposY;
+var fposX;
+var speed = 0.05;
+const pos = []
+var id = null;
+
 function init() {
-    ctx.clearRect(0,0,canvas.width, canvas.height);
-    drawLine(ctx, posX, posY, 100, 100, 'tomato', 3);
+    let i = 0;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    while (i < 10) {
+        iposY = Math.floor(Math.random() * 300 - 50);
+        iposX = Math.floor(Math.random() * 600 + 50);
+        lineLength = 200;
+        fposY = Math.cos(-30 * Math.PI / 180) * lineLength + iposY;
+        fposX = Math.sin(-30 * Math.PI / 180) * lineLength + iposX;
+        pos.push([iposX, iposY, fposX, fposY]);
+        i++;
+    }
+    function animate(direction,starttim,fintim) {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+        pos.forEach(element => {
+            element[0] -= direction;
+            element[1] -= direction;
+            element[2] += direction;
+            element[3] += direction;
+            drawLine(ctx, element[0], element[1], element[2], element[3], 'tomato', 3);
+        });
+
+        if(starttim != fintim){
+            starttim++;
+        }
+        if(starttim == fintim){
+            direction *=-1;
+            starttim =0;
+        }
+        console.log(direction);
+        requestAnimationFrame(function () {
+            animate(direction,starttim,fintim);
+        });
+    }
+    animate(speed,0,500);
+
+
 }
 function drawLine(ctx, x1, y1, x2, y2, stroke = 'black', width = 3) {
     // start a new path
